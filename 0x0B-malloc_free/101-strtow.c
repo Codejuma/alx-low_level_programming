1e230f2f35ee4c418bc3c39be395b56bd5fe73ee
ONGORO <jumaongoro5@gmail.com>
2023-06-13 18:26:32 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,10 +14,8 @@ int strncat_mod(char *dest, char *src, int i, int str_len)
 	int j;
 
 
-	for (j = 0; src[i] != ' ' && src[i] != '\0' && i < str_len; i++, j++)
-	{
+	for (j = 0; src[i] != ' ' && i < str_len; i++, j++)
 		dest[j] = src[i];
-	}
 
 	return (i);
 }
