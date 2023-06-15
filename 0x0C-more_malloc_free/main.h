3eddc7d0a37a54e83c8f28e3d2de1ebadd05aa3a
ONGORO <jumaongoro5@gmail.com>
2023-06-15 14:09:34 -0700

    main.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,17 @@
+#define _MAIN_H_
+#ifndef _MAIN_H_
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *create_xarray(int size);
+int find_len(char *str);
+char *iterate_zeroes(char *str);
+int get_digit(char c);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+int main(int argc, char *argv[]);
+#endif
