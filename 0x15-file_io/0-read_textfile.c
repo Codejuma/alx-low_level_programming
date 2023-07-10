ff42c02626c4983c7ab3f94f5cac69d4c540cfd5
Codejuma <jumaongoro5@gmail.com>
2023-07-10 10:20:26 -0700

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * read_textfile - text file and prints it to the
+ * POSIX standard output
+ * @filename: file name
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ * if filename is NULL return 0, if write fails or
+ * does not write the expected amount of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_ nr, nw;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * (letter));
+	if (!buf)
+		return (0);
+
+	nr = read(fd, buf, letters);
+	nw = write(STDOUT_FILENO, buf, nr);
+
+	close(fd);
+
+	free(buf);
+
+	return (nw);
+}
